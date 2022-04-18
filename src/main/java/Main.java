64f3f7c61bfe3ee8c06e242faf867719d12d990b
Kovalev <test@gmail.ru>
2022-04-18 17:38:11 +0300

    Second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,11 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.IOException;
+import java.net.URL;
 
 public class Main {
     public static void main(String[] args) {
-        String site = "https://en.wikipedia.org/";
+        String site = "https://auto.drom.ru/";
         try {
             parse(site);
         } catch (IOException e) {
@@ -15,13 +16,20 @@ public class Main {
     }
 
     static void parse(String url) throws IOException {
-        Document doc = Jsoup.connect(url).get();
+        Document doc = Jsoup.parse(new URL(url).openStream(), "windows-1251", url);
         System.out.println(doc.title());
-        Elements newsHeadlines = doc.select("#mp-itn b a");
-        for (Element headline : newsHeadlines) {
-            System.out.printf("%s\n\t%s",
-                    headline.attr("title"),
-                    headline.absUrl("href"));
+
+        System.out.println("\nМарки машин:");
+        Elements carItems = doc.select(".css-u4n5gw span[data-ftid='component_cars-list-item_name']");
+        for (Element car_item: carItems) {
+            System.out.println(car_item.html());
+        }
+        System.out.println("\nОбъявления:");
+        Elements adItems = doc.select(".css-1173kvb a[data-ftid='bulls-list_bull']");
+        for (Element ad_item: adItems) {
+            String title = ad_item.select("span[data-ftid='bull_title']").first().text();
+            String price = ad_item.select("span[data-ftid='bull_price']").first().text();
+            System.out.println(title + " - " + price + "p.");
         }
     }
 }
